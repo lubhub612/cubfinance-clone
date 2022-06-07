579cc7c5b38184994c282df0f9c1b261a4ade14b
lubhub612 <lundhakgithub@gmail.com>
2022-06-07 17:02:42 +0530

    first commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,123 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://pancakeswap.finance/swap?outputCurrency=0x50d809c74e0b8e49e7b4c65bb3109abe3ff4c1c1',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://pancakeswap.finance/add/0xe9e7cea3dedca5984780bafc599bd69add087d56/0x50d809c74e0b8e49e7b4c65bb3109abe3ff4c1c1',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Dens',
+    icon: 'PoolIcon',
+    href: '/dens',
+  },
+  {
+    label: 'Kingdoms',
+    icon: 'PawIcon',
+    href: '/kingdoms',
+    status: {
+      text: 'AUTO',
+      color: 'warning',
+    },
+  },
+  {
+    label: 'Airdrop',
+    icon: 'PawIcon',
+    href: '/airdrop',
+    status: {
+      text: 'POLYCUB',
+      color: 'warning',
+    },
+  },
+  {
+    label: 'PolyCUB',
+    icon: 'PawIcon',
+    href: 'https://polycub.com',
+    status: {
+      text: 'Polygon',
+      color: 'warning',
+    },
+  },
+  {
+    label: 'IDO',
+    icon: 'IfoIcon',
+    href: '/ido',
+  },
+  {
+    label: 'LeoBridge',
+    icon: 'NftIcon',
+    href: "https://bridge.cubdefi.com",
+  },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'GitHub',
+        href: 'https://github.com/CubFinance',
+      },
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x50d809c74e0b8e49e7b4c65bb3109abe3ff4c1c1',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/cub-finance/',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/cub-finance',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x50d809c74e0b8e49e7b4c65bb3109abe3ff4c1c1',
+      },
+    ],
+  },
+  {
+    label: 'Docs',
+    icon: 'TicketIcon',
+    href: 'https://docs.cubdefi.com/',
+  },
+  {
+    label: 'Roadmap',
+    icon: 'MoreIcon',
+    href: 'https://docs.cubdefi.com/roadmap',
+  },
+  {
+    label: "Tokenized Blogging",
+    icon: "TokenizeIcon",
+    href: "https://leofinance.io/",
+  },
+  {
+    label: "Blog",
+    icon: "BlogIcon",
+    href: "https://leofinance.io/@leofinance",
+  },
+  {
+    label: 'CertiK Audit',
+    icon: 'AuditIcon',
+    href: 'https://www.certik.org/projects/cubfinance',
+  },
+]
+
+export default config
